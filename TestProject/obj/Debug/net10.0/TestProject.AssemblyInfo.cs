035dae84827b8deba997755e29bc424c9dcb2524
Suryaditya Kumar Taneja <suryaditya.taneja@gmail.com>
2026-04-26 16:34:22 +0530

    Added boolena logic.

--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f8d8673d97d63100f5272a6b27cf76322afc9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b074e18ffb52ef05faafae0dfea973bb1cbd0598")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
