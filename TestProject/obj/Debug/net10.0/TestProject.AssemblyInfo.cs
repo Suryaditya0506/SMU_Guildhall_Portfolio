53f8d8673d97d63100f5272a6b27cf76322afc9f
Suryaditya Kumar Taneja <suryaditya.taneja@gmail.com>
2026-04-26 11:38:01 +0530

    Made changes to the Visual Studio extension.

--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4b0302e3e2de45875924f7b8fb4e2a03220893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea59c7fd6d213e575d69826076835fd879bb850")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
